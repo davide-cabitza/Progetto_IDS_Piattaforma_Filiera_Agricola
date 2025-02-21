5f074f140f39ea60f8c2eb53d71b6e3f0013ee6c
Diego Babini <diegobabi03@gmail.com>
2025-02-21 17:19:12 +0100

    modifica iniziale
    modifica iniziale

---
+++ src/main/java/unicam/piattaforma_filiera_agricola/PiattaformaFilieraAgricolaApplication.java
@@ -0,0 +1,13 @@
+package unicam.piattaforma_filiera_agricola;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiattaformaFilieraAgricolaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PiattaformaFilieraAgricolaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/unicam/piattaforma_filiera_agricola/PiattaformaFilieraAgricolaApplicationTests.java
@@ -0,0 +1,13 @@
+package unicam.piattaforma_filiera_agricola;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiattaformaFilieraAgricolaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
