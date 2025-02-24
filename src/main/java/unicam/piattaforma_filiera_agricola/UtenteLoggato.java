0b35affa13390f4a4ee5f25489a73cce23c7e4af
Diego Babini <diegobabi03@gmail.com>
2025-02-24 22:08:36 +0100

    Create UtenteLoggato.java

---
+++ src/main/java/unicam/piattaforma_filiera_agricola/UtenteLoggato.java
@@ -0,0 +1,43 @@
+package unicam.piattaforma_filiera_agricola;
+
+public abstract class UtenteLoggato {
+    private int id;
+    private String nome;
+    private String email;
+    private int numeroTelefono;
+    private String indirizzo;
+    private String NomeUtente;
+    private String password;
+
+    public UtenteLoggato() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getIndirizzo() {
+        return indirizzo;
+    }
+
+    public int getNumeroTelefono() {
+        return numeroTelefono;
+    }
+
+    public String getNomeUtente() {
+        return NomeUtente;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
