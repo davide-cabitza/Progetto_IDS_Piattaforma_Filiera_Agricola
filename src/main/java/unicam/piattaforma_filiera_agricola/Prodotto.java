fb719f0836264ece34b31484fdb5168f8362adfb
Diego Babini <diegobabi03@gmail.com>
2025-02-24 16:09:20 +0100

    Update Prodotto.java

--- src/main/java/unicam/piattaforma_filiera_agricola/Prodotto.java
+++ src/main/java/unicam/piattaforma_filiera_agricola/Prodotto.java
@@ -1,6 +1,15 @@
 package unicam.piattaforma_filiera_agricola;
 
 public class Prodotto {
+    private int id;
     private String nomeProdotto;
     private String descrizione;
+    private String certificati;
+    private double costo;
+    private int quantitativo;
+    private String categoria;
+    private int id_venditore;
+    private long dataInserimento;
+
+
 }
