fe4798a45d0f216cec01048816b26de4a5a0b9e7
Fed2203 <federico.massani@studenti.unicam.it>
2025-02-25 13:26:51 +0100

    Update Produttore.java

--- src/main/java/unicam/piattaforma_filiera_agricola/Produttore.java
+++ src/main/java/unicam/piattaforma_filiera_agricola/Produttore.java
@@ -1,18 +1,32 @@
 package unicam.piattaforma_filiera_agricola;
 
 import java.util.Date;
+import java.util.List;
 
 public class Produttore extends Venditore {
 
-    
 
     public Produttore(int id, String nome, String email, int numeroTelefono, String indirizzo, String NomeUtente, String password,String partitaIva) {
+
+    public String processoColtivazione;
+
+
+    public Produttore(int id, String nome, String email, int numeroTelefono, String indirizzo, String NomeUtente, String password,String partitaIva,String processoColtivazione) {
         super(id,nome,email,numeroTelefono,indirizzo,NomeUtente,password,partitaIva);
+        this.processoColtivazione=processoColtivazione;
     }
 
     @Override
     public Prodotto creaProdotto(String id, String nome, String descrizione, Float costo, int quantitativo, Date data, CategoriaProdotto categoriaProdotto, Certificati certificatiProdotto) {
-return null;
+        return null;
+    }
+
+    public String getProcessoColtivazione() {
+        return processoColtivazione;
+    }
+
+    public void setProcessoColtivazione(String processoColtivazione) {
+        this.processoColtivazione = processoColtivazione;
     }
 
 
