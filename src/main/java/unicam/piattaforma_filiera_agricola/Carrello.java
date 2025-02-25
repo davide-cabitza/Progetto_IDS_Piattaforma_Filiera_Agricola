b0a29357a557768d4f4f22f43f06c949a46dc71f
Fed2203 <federico.massani@studenti.unicam.it>
2025-02-25 18:27:20 +0100

    agg

--- src/main/java/unicam/piattaforma_filiera_agricola/Carrello.java
+++ src/main/java/unicam/piattaforma_filiera_agricola/Carrello.java
@@ -1,6 +1,21 @@
 package unicam.piattaforma_filiera_agricola;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Carrello {
-    Prodotto[] listaProdotti;
-    PacchettoProdotti[] listaPacchettoProdotto;
+    private List<Prodotto> prodotti;
+    private Acquirente acquirente;
+    private List<PacchettoProdotti> pacchetti;
+
+    public Carrello(Acquirente acquirente) {
+        this.acquirente = acquirente;
+        this.prodotti = new ArrayList<>();
+        this.pacchetti = new ArrayList<>();
+    }
+
+
+
+
+
 }
