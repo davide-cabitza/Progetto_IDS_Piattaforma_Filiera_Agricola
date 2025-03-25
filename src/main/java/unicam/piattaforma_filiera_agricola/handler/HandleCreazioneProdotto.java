171dc03a8043721ddeb977f68c550be60ca52fc2
Davide Cabitza <davide.cabitza@studenti.unicam.it>
2025-03-25 15:57:56 +0100

    aggiunta Handler

---
+++ src/main/java/unicam/piattaforma_filiera_agricola/handler/HandleCreazioneProdotto.java
@@ -0,0 +1,8 @@
+package unicam.piattaforma_filiera_agricola.handler;
+
+import unicam.piattaforma_filiera_agricola.product.Prodotto;
+
+public class HandleCreazioneProdotto {
+
+    public void creaProdotto(Prodotto prodotto) {}
+}
