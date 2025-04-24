d9dbea4f2d18556313a4bb098946ef681d78d888
Diego Babini <diego.babini@studenti.unicam.it>
2025-04-24 19:33:04 +0200

    Modifca Prodotto/Venditore

--- src/main/java/unicam/piattaforma_filiera_agricola/model/product/Prodotto.java
+++ src/main/java/unicam/piattaforma_filiera_agricola/model/product/Prodotto.java
@@ -1,38 +1,57 @@
 package unicam.piattaforma_filiera_agricola.model.product;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import jakarta.persistence.*;
+import org.springframework.context.annotation.EnableMBeanExport;
 import unicam.piattaforma_filiera_agricola.Acquirente;
-
 import java.util.Date;
 
+@JsonTypeInfo(
+        use = JsonTypeInfo.Id.NAME,
+        include = JsonTypeInfo.As.PROPERTY,
+        property = "tipo"
+)
+@JsonSubTypes({
+        @JsonSubTypes.Type(value = ProdottoProduttore.class, name = "produttore"),
+        @JsonSubTypes.Type(value = ProdottoTrasformatore.class, name = "trasformatore"),
+        @JsonSubTypes.Type(value = ProdottoDistributore.class, name = "distributore")
+})
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "tipo_prodotto", discriminatorType = DiscriminatorType.STRING)
 public class Prodotto {
-    private int id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
     private String nomeProdotto;
-    private String descrizione;
-    private String certificati;
     private double costo;
+    private String descrizione;
+
+    private String certificati;
     private int quantitativo;
     private String categoria;
     private int id_venditore;
     private Date dataInserimento;
-    private Acquirente creatore;
 
-    public Prodotto(int id, String nomeProdotto, String descrizione, String certificati, double costo, int quantitativo, String categoria,int id_venditore, Date dataInserimento) {
-        this.id = id;
+    public Prodotto(String nomeProdotto, String descrizione, String certificati, double costo, int quantitativo, String categoria,int id_venditore, Date dataInserimento) {
         this.nomeProdotto = nomeProdotto;
+        this.costo = costo;
         this.descrizione = descrizione;
         this.certificati = certificati;
-        this.costo = costo;
         this.quantitativo = quantitativo;
         this.categoria = categoria;
         this.id_venditore = id_venditore;
         this.dataInserimento = dataInserimento;
     }
 
-    public int getId() {
+public Prodotto() {}
+
+    public long getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(long id) {
         this.id = id;
     }
 
@@ -101,7 +120,4 @@ public class Prodotto {
     }
 
 
-    public Acquirente getCreatore() {
-        return creatore;
-    }
 }
