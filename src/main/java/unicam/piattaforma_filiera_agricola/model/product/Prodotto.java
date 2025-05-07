1ba940ab5c558de49ffb99abefcddcdba483a916
Diego Babini <diego.babini@studenti.unicam.it>
2025-05-07 21:50:30 +0200

    Prodotto

--- src/main/java/unicam/piattaforma_filiera_agricola/model/product/Prodotto.java
+++ src/main/java/unicam/piattaforma_filiera_agricola/model/product/Prodotto.java
@@ -79,11 +79,11 @@ public class Prodotto {
         this.descrizione = descrizione;
     }
 
-    public List<String> getCertificati() {
+    public String getCertificati() {
         return certificati;
     }
 
-    public void setCertificati(List<String> certificati) {
+    public void setCertificati(String certificati) {
         this.certificati = certificati;
     }
 
